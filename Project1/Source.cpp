2f90e21786c9aedc24be783e5f82dddbf8c12750
Alona <alyona2012951@gmail.com>
2023-09-18 10:05:49 +0300

    smth

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 // github project blackjack
 
 int main() {
 
-	std::cout << "Smth" << std::endl;
+	std::vector <std::string> {"Clubs", "Hearts", "Diamodns", "Spades"};
 }
